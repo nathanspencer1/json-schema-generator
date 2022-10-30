ace67c07a78d464d7a1172cf0df50a48ea7101d2
Nathan Spencer <nathanspencer@conservice.com>
2022-10-30 17:19:39 -0600

    getting rid or default react stuff.

--- src/App.js
+++ src/App.js
@@ -4,32 +4,20 @@ import httpGet from './logic/JsonSchema';
 import { useEffect, useState } from 'react';
 
 function App() {
-  const [state, setState] = useState({ swaggerContent: "" });
-  useEffect(() => {
-    async function fetchData() {
-      const response = await httpGet("https://petstore.swagger.io/v2/swagger.json");
-      setState({ swaggerContent: response });
-      console.log(response);
-    }
-    fetchData();
-  }, []);
+  const [swaggerJsonURL, setSwaggerJsonURL] = useState("https://petstore.swagger.io/v2/swagger.json");
+  const [swaggerContent, setSwaggerContent] = useState("");
+  // useEffect(() => {
+  //   async function fetchData() {
+  //     const response = await httpGet(swaggerJsonURL);
+  //     setSwaggerContent(response);
+  //     console.log(response);
+  //   }
+  //   fetchData();
+  // }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <textarea cols={100} rows={100} value={state.swaggerContent} />
+      <input type={'text'} value={swaggerJsonURL} size={200} />
+      <textarea cols={100} rows={100} value={swaggerContent} />
     </div>
   );
 }
